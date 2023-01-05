292fcbc8de6d6858220f63c23becde71851ee97d
a-shev <alexshevlin1@gmail.com>
2023-01-05 01:28:18 -0800

    WIP about page.

---
+++ src/Components/About.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function About() {
+  return(
+    <React.Fragment>
+      <div className="w-3/4 mx-auto">
+        <div className="my-5 mx-auto bg-gradient-to-b from-blue-600 dark:from-sky-500 rounded-full w-64 h-64 relative">
+        </div>
+        <h4 className="text-xl text-left font-semibold py-3">My Experience</h4>
+        <p className="leading-7 py-4">I am a skilled Full Stack Developer with 2 years of experience in this field. I am proficient in C#, React, JavaScript, and Python, and have experience in Dev Ops and Cloud Development. I am detail-oriented and able to effectively plan and execute projects from start to finish, ensuring that all deadlines are met and that all stakeholders are kept informed. Overall, I am a valuable asset to any development team, with a strong combination of technical and managerial skills.</p>
+
+        <p className="leading-7 py-4">In addition to my technical skills, I have 3 years of experience across Project Management and Marketing Management. I have a proven track record of leading teams, and are skilled in team building and communication. I am able to effectively manage projects from start to finish, ensuring that all deadlines are met and all stakeholders are kept informed. I am detail-oriented and able to effectively plan and execute marketing campaigns, driving results for their organization.</p>
+
+        <p className="leading-7 py-4">Overall, I hope to be valuable asset to any team, with a combination of technical, managerial, and marketing expertise.</p>
+      </div>
+    </React.Fragment>
+  )
+}
