49ca3448f60b066e240364f958189bc8a5b74180
a-shev <alexshevlin1@gmail.com>
2023-01-25 14:00:36 -0800

    update project hover state and click.

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -4,19 +4,16 @@ import PropTypes from "prop-types";
 function Project(props) {
   return(
     <React.Fragment>
-      <div className="w-full h-full mx-auto p-1 overflow-hidden relative cursor-pointer hover:brightness-125 dark:hover:brightness-75">
+      <div className="w-full h-full mx-auto p-1 overflow-hidden relative cursor-pointer">
         { props.img.includes("png") ? 
-        <a rel="noreferrer" href={props.url} target="_blank">
           <img className="object-cover w-full min-h-52 h-80 rounded-lg" src={props.img} alt={props.id}/>
-        </a>
         :
-        <a rel="noreferrer" href={props.url} target="_blank">
         <video className="object-cover w-full min-h-52 h-80 rounded-lg" controls muted>
           <source src={props.img} type="video/mp4" />
         </video>
-        </a>
         }
-          <div class="w-full p-2.5 top-0 inset-x-0 text-blue-800 dark:text-slate-200 text-xl text-left select-none font-semibold">
+        <a rel="noreferrer" href={props.url} target="_blank">
+          <div class="w-full p-2.5 top-0 inset-x-0 text-blue-500 dark:text-slate-200 text-xl text-left select-none font-semibold :hover:brightness-75 dark:hover:text-sky-400 hover:text-blue-700">
             <p className="tracking-wide">
               {props.name}
             </p>
@@ -24,6 +21,7 @@ function Project(props) {
               {props.language}  
             </p>
           </div>
+        </a>
       </div>
     </React.Fragment>
   )
