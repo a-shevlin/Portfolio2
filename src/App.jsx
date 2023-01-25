7add660f78d3b96fd66e5e9cdafc1c115745b095
a-shev <alexshevlin1@gmail.com>
2023-01-25 14:00:46 -0800

    embed about component.

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import {
   AiFillLinkedin,
   AiFillGithub, 
 } from "react-icons/ai";
-import { Nav, Project } from "./Components";
+import { Nav, Project, About } from "./Components";
 import { projects } from "./data";
 import alex from "./assets/alex-headshot.png"
 
@@ -16,7 +16,7 @@ function App() {
         <h4 className="text-xl text-left">Hi, my name is</h4>
         <h1 className="text-5xl text-blue-500 dark:text-sky-400">Alex Shevlin</h1>
         <h3 className="text-4xl py-2 opacity-70">I'm a Full Stack Developer </h3>
-        <h5 className="text-sm font-normal leading-6 pb-5">currently focused on making websites and applications more accessible by everyday people. Reach out to me if you're interested in working together!</h5>
+        <h5 className="text-sm font-normal leading-6 pb-5">Currently focused on making websites and applications more accessible by everyday people. Reach out to me if you're interested in working together!</h5>
         <div className="text-4xl gap-16 flex mx-auto text-center dark:text-gray-300">
           <a className="ml-auto" href="https://www.linkedin.com/in/alexshevlin/" rel="noreferrer" target="_blank">
             <AiFillLinkedin className="hover:dark:text-sky-500 hover:text-blue-600"/>
@@ -30,11 +30,10 @@ function App() {
         <div className="mx-auto bg-gradient-to-b from-blue-600 dark:from-sky-500 rounded-full w-64 h-64 relative overflow-hidden">
           <img src={alex} alt="an image of Alex Shevlin" className="fill cover"/>
         </div>
-        <h4 className="text-xl text-left font-semibold pt-3">My Experience</h4>
-        <p className="leading-[1.6rem] py-4 text-left">I am a skilled Full Stack Developer with 2 years of experience in this field. I am proficient in C#, React, JavaScript, and Python, and have experience in Dev Ops and Cloud Development. I am detail-oriented and able to effectively plan and execute projects from start to finish, ensuring that all deadlines are met and that all stakeholders are kept informed. Overall, I am a valuable asset to any development team, with a strong combination of technical and managerial skills.</p>
+        <About />
       </div>
       <div className="w-3/4 mx-auto h-full flex flex-col">
-        <h4 className="text-xl text-left font-semibold pb-3">Portfolio Projects</h4>
+        <h4 className="text-xl text-left font-semibold pb-3">Full Stack Projects</h4>
         <div className="mx-auto w-full tracking-narrow drop-shadow-lg font-bold p-0 text-center uppercase grid grid-cols-1 lg:grid-cols-2 lg">
           {Object.values(projects).map((project) => (
             <Project
