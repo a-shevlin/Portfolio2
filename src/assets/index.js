31f10e2fde043ea823f94af60f57ae87ab400fd6
Alex Shevlin <alexshevlin1@gmail.com>
2024-12-08 01:28:25 -0800

    fix resume download naming scheme.

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,4 +2,4 @@ export { default as ag } from "./anaheim-grease.png";
 export { default as slag } from "./slag.png";
 export { default as visualizer } from "./visualizer.png";
 export { default as dcweb } from "./dc-website.png";
-export { default as resume } from "./resume.pdf";
+export { default as resume } from "./AlexShevlin-SoftwareEngineer.pdf";
