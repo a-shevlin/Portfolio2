03244e26e753c877ff552d1c21647a10d08ce2e0
Alex Shevlin <alexshevlin1@gmail.com>
2024-12-08 01:17:20 -0800

    update resume

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,18 +1,17 @@
 import React from "react";
-import { BsMoonStarsFill } from 'react-icons/bs';
-import resume from "../assets/Alex Shevlin Resume.pdf";
+import { resume } from "../assets";
 
 function Nav() {
   return(
     <React.Fragment>
-      <nav className="relative mx-auto h-8 z-40">
-        <div className="w-full fixed px-8 py-3 top-0 left-1/2 -translate-x-1/2 uppercase  bg-[conic-gradient(at_top_right,_var(--tw-gradient-stops))]  from-blue-200 via-pink-200 to-amber-200 dark:from-sky-800 dark:via-[#000000] dark:to-[#2348c4]">
-          <div className="w-10/12 mx-auto flex justify-between">
+      <nav className="relative mx-auto h-8 z-40 w-full">
+        <div className="w-full fixed px-8 py-3 top-0 left-1/2 -translate-x-1/2 uppercase">
+          <div className="w-8/12 mx-auto flex justify-between">
 
-          <h1 className="font-bold tracking-[0.3em] my-auto scale-y-110">as</h1>
+          <h1 className="text-3xl font-bold tracking-[-0.1em] my-auto scale-y-110 lowercase">as</h1>
           <ul className="flex items-center gap-4">
             <li>
-              <a href={resume} className="py-1 rounded-lg text-slate-100 bg-gradient-to-r from-blue-500 to-red-600 dark:from-sky-400 dark:to-slate-100 bg-clip-text text-2xl text-transparent font-bold animate-text" download>
+              <a href={resume} className="py-1 rounded-lg text-slate-100 bg-gradient-to-r from-blue-500 to-red-600 dark:from-sky-400 dark:to-slate-100 bg-clip-text text-2xl text-transparent font-bold animate-text" rel="noreferrer" target="_blank">
                 Resume
               </a>
             </li>
---
+++ src/assets/index.js
@@ -0,0 +1,6 @@
+export { default as dawn } from "./dawn.png";
+export { default as ag } from "./anaheim-grease.png";
+export { default as slag } from "./slag.png";
+export { default as visualizer } from "./visualizer.png";
+export { default as dcweb } from "./dc-website.png";
+export { default as resume } from "./resume.pdf";
